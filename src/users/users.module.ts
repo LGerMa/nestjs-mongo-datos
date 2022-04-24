ed7c4d37bfafc340556c73d9916cbd88938a0d69
Luis Martinez <luis.martinez@hugoapp.com>
2022-04-23 23:11:42 -0600

    feat: customer entity mongoose schema

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { CustomerController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
@@ -7,8 +8,18 @@ import { UsersService } from './services/users.service';
 
 import { ProductsModule } from '../products/products.module';
 
+import { Customer, CustomerSchema } from './entities/customer.entity';
+
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forFeature([
+      {
+        name: Customer.name,
+        schema: CustomerSchema,
+      },
+    ]),
+  ],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
